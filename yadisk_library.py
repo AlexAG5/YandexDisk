fc18a3b71bcb68b0f8dc7bc04a944c4ac281716d
Alexey Goncharuk <code.cat97@gmail.com>
2023-03-10 15:27:45 +0300

    Initial commit

---
+++ yadisk_library.py
@@ -0,0 +1,13 @@
+import yadisk
+import requests
+from pprint import pprint
+TOKEN = input(str("Введите токен: "))
+y = yadisk.YaDisk(token=TOKEN)
+if y.check_token():
+    if not y.is_dir("/Нетология"):
+        y.mkdir("/Нетология")
+        print('Папка "Нетология" создана')
+if y.upload('test_upload.txt', '/Нетология/test_upload.txt'):
+    pprint('Файл успешно загружен')
+else:
+    print("Ошибка")
